a73acd35bfca76fed989a0d675128b1776433994
Nicholas Leo <n.b.leo@protonmail.com>
2022-01-13 20:28:20 -0600

    Added support for uppercase letters and punctuation.

--- cipher.rb
+++ cipher.rb
@@ -1,11 +1,23 @@
+require 'pry-byebug'
+
+
 def caesar_cipher(string, shift)
     comp_string = 'abcdefghijklmnopqrstuvwxyz'.split('')
-    new_string_array = string.split('').map do |char|
-        new_char_index = comp_string.index(char) + shift - 26
-        comp_string[new_char_index]
+    upper_index = string.split('').map.with_index { |char, i| i if (char == char.upcase)}.compact
+    shift = shift % 25 - 26
+    
+    new_string = string.split('').map.with_index do |char, i|
+        if comp_string.include?(char.downcase)
+            if upper_index.include?(i)
+                comp_string[comp_string.index(char.downcase) + shift].upcase
+            else
+                comp_string[comp_string.index(char.downcase) + shift]
+            end
+        else
+            char
+        end
     end
-
-    new_string_array.join('')
+    new_string.join('')
 end
 
-p caesar_cipher('xxx', 10)
+p caesar_cipher('Roger that captain!', 24)
